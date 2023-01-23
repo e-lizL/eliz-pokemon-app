9253347d525f082befdd8287b59432c21330d8d2
elizabeth lands <elizabeth_lands@hotmail.com>
2023-01-23 15:07:57 +0000

    feat: add higher quality image to pokemon cards

--- src/components/PokemonCards.js
+++ src/components/PokemonCards.js
@@ -1,8 +1,7 @@
 import { v4 as uuidv4 } from 'uuid';
 import styled from 'styled-components';
 
-const StyledPokemonCards = styled.div`
-  /* border: 1px solid green;   */
+const StyledPokemonCards = styled.div`   
   width: 98%;
   margin: 0 auto;
   display: grid;
@@ -20,6 +19,15 @@ const PokemonCard = styled.div`
   padding: 12px;
 `;
 
+const StyledPokemonImageWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  img{ 
+    height: 70px;
+    width: 70px;
+    }
+`;
+
 const PokemonName = styled.div`
   margin-top: 8px;
 `
@@ -30,12 +38,14 @@ const PokemonCards = ({ pokemonData }) => {
    <StyledPokemonCards>
        { pokemonData.map(pokemon => (
           <PokemonCard key={uuidv4()}>
-            <img 
-              src={pokemon.sprites.front_default}
-              alt="placeholder"
-              width="150"
-              height="150" 
-            />
+            <StyledPokemonImageWrapper>
+              <img 
+                src={pokemon.sprites.other.dream_world.front_default}
+                alt="placeholder"
+                width="150"
+                height="150" 
+              />
+            </StyledPokemonImageWrapper>
             <PokemonName>{pokemon.name}</PokemonName> 
           </PokemonCard>
       ))} 
