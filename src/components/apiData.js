b94d08c117c43d92df85aa04baa961a7bbc6c112
elizabeth lands <elizabeth_lands@hotmail.com>
2023-01-23 15:07:56 +0000

    feat: console log data

--- src/components/apiData.js
+++ src/components/apiData.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
+import { v4 as uuidv4 } from 'uuid';
 
 const ApiData = () => {
   const [pokemonData, setPokemonData] = useState([]);
@@ -7,17 +8,20 @@ const ApiData = () => {
 
   useEffect(() => {
     axios
-      .get(pokemonUrl)
-      .then(response => setPokemonData(response.data))
-      .catch(error => console.log(error));
-  }, []);
-
+    .get(pokemonUrl)
+    .then((response) => {setPokemonData(response.data);
+    });
+   }, []);
+    
+   console.log(pokemonData.results);
   return (
    <>
-      { pokemonData.map(pokemon => (
-        <div key={"name"}>
+      {/* { pokemonData.map(pokemon => (
+        <div key={uuidv4()}>
+          <div>{pokemon.name}</div>
+          <div>{pokemon.url}</div>
         </div>
-      ))}
+      ))} */}
     </>   
   )
 };
