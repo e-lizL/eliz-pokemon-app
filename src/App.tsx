5e76438cbf4828c3ce7adbe5d5ad75e781864f19
elizabeth lands <elizabeth_lands@hotmail.com>
2023-01-23 15:07:56 +0000

    feat: basic layout of main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,38 @@
+import {
+  HeaderWrapper,
+  CardContainer,
+  Divider,
+  OuterCircle,
+  InnerCircle,
+  LeftSelect,
+  RightSelect,
+  StyledSelectLabel,
+  StyledSelect,
+  StyledSelectWrapper,
+} from "./AppStyles";
+
 function App() {
-  return <h1>Hello from React</h1>;
+  return (
+    <>
+      <HeaderWrapper>
+        <CardContainer>
+          <h1>Hello from React</h1>
+        </CardContainer>
+      </HeaderWrapper>
+      <Divider>
+        <OuterCircle>
+          <InnerCircle>
+            <LeftSelect />
+            <RightSelect />
+          </InnerCircle>
+        </OuterCircle>
+      </Divider>
+      <StyledSelectWrapper>
+        <StyledSelectLabel>Choose your Pokemon:</StyledSelectLabel>
+        <StyledSelect />
+      </StyledSelectWrapper>
+    </>
+  );
 }
 
 export default App;
---
+++ src/AppStyles.js
@@ -0,0 +1,95 @@
+import styled from "styled-components";
+import { LeftArrow } from "@styled-icons/boxicons-solid/LeftArrow";
+import { RightArrow } from "@styled-icons/boxicons-solid/RightArrow";
+
+const HeaderWrapper = styled.div`
+  background: #ce2222;
+  border: 4px solid green;
+  padding: 80px 0 140px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const CardContainer = styled.div`
+  height: 200px;
+  margin-top: 50px;
+  margin: 0 auto;
+  background: white;
+  border-radius: 10px;
+  border: 2px solid black;
+  display: inline-block;
+  padding: 20px;
+`;
+
+const Divider = styled.div`
+  height: 40px;
+  background: black;
+`;
+
+const OuterCircle = styled.div`
+  width: 250px;
+  height: 250px;
+  border-radius: 100%;
+  margin: 0 auto;
+  border: 7px solid black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  z-index: 999;
+  backgound: white;
+`;
+
+const InnerCircle = styled(OuterCircle)`
+  width: 210px;
+  height: 210px;
+  border: 4px solid black;
+  display: flex;
+  justify-content: space-around;
+`;
+
+const LeftSelect = styled(LeftArrow)`
+  color: black;
+  height: 60px;
+`;
+
+const RightSelect = styled(RightArrow)`
+  color: black;
+  height: 60px;
+`;
+
+const StyledSelectLabel = styled.label`
+  font-size: 1.2rem;
+  padding-left: 10px;
+`;
+
+const StyledSelect = styled.select`
+  height: 45px;
+  width: 200px;
+  padding: 0 20px;
+  display: flex;
+  font-size: 1.2rem;
+  border-radius: 20px;
+  background: gray;
+  color: white;s
+`;
+
+const StyledSelectWrapper = styled.div`
+  display: flex;
+  gap: 10px;
+  align-items: center;
+`;
+
+export {
+  HeaderWrapper,
+  CardContainer,
+  Divider,
+  OuterCircle,
+  InnerCircle,
+  LeftSelect,
+  RightSelect,
+  StyledSelectLabel,
+  StyledSelect,
+  StyledSelectWrapper,
+};
